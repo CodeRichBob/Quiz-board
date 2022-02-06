1230b6c0e7678157e6452c783ebefe8666205ffc
CodeRichBob <rgitonga07@gmail.com>
2022-02-07 01:43:34 +0300

    ft:added variables-created variables to store correct answer from quiz

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function mySubmitTest(){
+
+   var result = 0;
+   var answer1 = document.Questions.question1.value;
+   var answer2 = document.Questions.question2.value;
+   var answer3 = document.Questions.question3.value;
+   var answer4 = document.Questions.question4.value;
+   var answer5 = document.Questions.question5.value;
+
+   if (answer1 == ".js"){result+=20};
+}
