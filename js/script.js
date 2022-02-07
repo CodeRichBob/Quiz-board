2c930388f580f731358db1fc30f1a2c138ca12b9
CodeRichBob <rgitonga07@gmail.com>
2022-02-07 04:12:36 +0300

    refactor:check onclick function-confirm function is well written

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-function mySubmitTest(){
+function mySubmitTestbtn (){
 
    var result = 0;
-   var answer1 = document.Questions.question1.value;
-   var answer2 = document.Questions.question2.value;
-   var answer3 = document.Questions.question3.value;
-   var answer4 = document.Questions.question4.value;
-   var answer5 = document.Questions.question5.value;
+   var selectedAnswer1 = document.Questions.question1.value;
+   var selectedAnswer2 = document.Questions.question2.value;
+   var selectedAnswer3 = document.Questions.question3.value;
+   var selectedAnswer4 = document.Questions.question4.value;
+   var selectedAnswer5 = document.Questions.question5.value;
+   var score = document.getElementById ("score");
 
-   if (answer1 == ".js"){result+=20};
-   if (answer2 == "Script tag"){result+=20};
-   if (answer3 == "Both A & B"){result+=20};
-   if (answer4 == "Array"){result+=20};
-   if (answer5 == "Method"){result+=20};
+   if (selectedAnswer1 == ".js"){result+=20};
+   if (selectedAnswer2 == "Script tag"){result+=20};
+   if (selectedAnswer3 == "Both A & B"){result+=20};
+   if (selectedAnswer4 == "Array"){result+=20};
+   if (selectedAnswer5 == "Method"){result+=20};
    Questions.style.display = 'none';
 
-   
-}
+   if (result == 100){
+      score.textContent = "Your Score is 100%. Excellent!";
+   } else if (result == 80){
+      score.textContent = "Your Score is 80%.Keep it up!";
+     } else if (result == 60){
+      score.textContent = "Your Score is 60%.There's room for improvement!";
+     }else if (result == 40){
+      score.textContent = "Your Score is 40%.You can do better!";
+     }else if (result == 20){
+      score.textContent = "Your Score is 20%. This is below average!";
+     }else if (result == 0){
+      score.textContent = "Your Score is 0";
+     }
+};
