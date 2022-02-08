5d5c0f1ac9fcfb06e46e62d9e6d89ff5ffe6bbdb
CodeRichBob <rgitonga07@gmail.com>
2022-02-08 15:21:11 +0300

    ft:making button interactive-made changes to enable submitting answers

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function mySubmitTestbtn (){
+function mySubmitBtn (){
 
    var result = 0;
    var selectedAnswer1 = document.Questions.question1.value;
@@ -6,7 +6,7 @@ function mySubmitTestbtn (){
    var selectedAnswer3 = document.Questions.question3.value;
    var selectedAnswer4 = document.Questions.question4.value;
    var selectedAnswer5 = document.Questions.question5.value;
-   var score = document.getElementById ("score");
+   var score = document.getElementById("score");
 
    if (selectedAnswer1 == ".js"){result+=20};
    if (selectedAnswer2 == "Script tag"){result+=20};
@@ -14,9 +14,10 @@ function mySubmitTestbtn (){
    if (selectedAnswer4 == "Array"){result+=20};
    if (selectedAnswer5 == "Method"){result+=20};
    Questions.style.display = 'none';
-
+   document.getElementsByClassName('guidelines')[0].style.visibility = 'hidden';
+   document.getElementsByClassName('ready')[0].style.visibility = 'hidden';
    if (result == 100){
-      score.textContent = "Your Score is 100%. Excellent!";
+      score.textContent = "Your Score is 100%. Excellent! Your mastery for Javascript is amazing";
    } else if (result == 80){
       score.textContent = "Your Score is 80%.Keep it up!";
      } else if (result == 60){
@@ -26,6 +27,6 @@ function mySubmitTestbtn (){
      }else if (result == 20){
       score.textContent = "Your Score is 20%. This is below average!";
      }else if (result == 0){
-      score.textContent = "Your Score is 0";
+      score.textContent = "Your Score is 0. Kindly retake the test" ;
      }
-};
+}
